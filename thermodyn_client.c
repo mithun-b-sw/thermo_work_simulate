cb19b1b9c9b4113df270e4736983de12659c8136
Mithun B <mithunb2049@gmail.com>
2024-04-03 22:59:36 +0530

    Add iteration option to client code
    This change do not have any impact on library part.
    Now the command to test the library can be given with run
    with number of times the thermo-dynamic cycles to be ran
    as argument, Ex:
    
    $ ./thermodyn_cycle
    
    (or)
    
    $ ./thermodyn_cycle 10

--- thermodyn_client.c
+++ thermodyn_client.c
@@ -2,13 +2,20 @@
  * This is a simple test code to check the thermodyn library.
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include "thermodyn_helper.h"
 
 #define GAS_VOLUME		0.01
 #define GAS_PRESSURE	101325
 #define GAS_TEMPERATURE	300
 
-int main() {
+int main(int argc, char **argv) {
+
+	int iterations = 1;
+	if(argc == 2)
+	{
+		iterations = atoi(argv[1]);
+	}
 	/**
 	 * The 'device' type is an opaque pointer,
 	 * followed by init() and uninit() act as interface using the opaque pointer.
@@ -19,6 +26,9 @@ int main() {
 	printf("# At initial state\n");
 	print_current_state(gas);
 
+	for(int i = 0; i < iterations; ++i)
+	{
+
 	/**
 	 * Isometric heat addition from external source
 	 */
@@ -27,7 +37,7 @@ int main() {
 	print_current_state(gas);
 
 	/**
-	 * Isothermal expansion by heating from external source
+	 * Isothermal expansion by heat observed from external source
 	 */
 	update_volume_isothermal(gas, GAS_VOLUME * 1.2);
 	printf("# After increasing volume from 0.01 m³ to 0.012 m³\n");
@@ -43,9 +53,11 @@ int main() {
 	/**
 	 * Isothermal compression by rejecting heat
 	 */
-	update_volume_isothermal(gas, GAS_VOLUME);
+	update_volume_isothermal(gas, GAS_VOLUME * 0.8334);
 	printf("# After decreasing volume back from 0.012 m³ to 0.01 m³\n");
 	print_current_state(gas);
+
+	}
 
 	deinit(gas);
 	return 0;
